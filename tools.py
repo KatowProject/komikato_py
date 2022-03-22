f4a586f23be0f8fe504cc28452ef6bd09b1a85dc
KatowProject <naufalfaqih443@gmail.com>
2022-03-22 08:50:07 +0700

    bypass cf

--- tools.py
+++ tools.py
@@ -1,5 +1,6 @@
 import requests as r
 import base64
+import json
 
 req = r.Session()
 req.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'})
@@ -11,7 +12,10 @@ def get(url, options={}):
     if (status == 200):
         return response
     else:
-        # url to base64
         url_base64 = base64.b64encode(url.encode('utf-8'))
         response = req.get("https://bypass.kato-rest.us/url/" + url_base64.decode('utf-8'))
-        return response
+        if (response.status_code == 200 and json.loads(response.text)['status'] == 'error'):
+            if "komikindo" in url:
+                url = url.replace("komikindo.id", "komikindo-id.translate.goog")
+                response = req.get(url + "?_x_tr_sl=auto&_x_tr_tl=en&_x_tr_hl=id", params=options)
+                return response
